78de83d953451fb3a158ceb8886b2cfaf9f1e9c4
Angel Woo <awoo424@gmail.com>
2022-01-14 22:57:22 +0800

    Update rolling_join example

--- src/clojask_examples/rolling_join.clj
+++ src/clojask_examples/rolling_join.clj
@@ -1,1 +1,9 @@
 (ns clojask-examples.rolling-join)
+
+(defn main
+    []
+    (def x (clojask/dataframe "resources/employees.csv"))
+    (def y (clojask/dataframe "resources/employees-workleave.csv"))
+
+    (clojask/compute (clojask/rolling-join-forward x y ["Employee"] ["Employee"] "UpdateDate" "UpdateDate") 8 "resources/output.csv" :exception false)
+    )
