845f305b0805fe2120f718a5e09ef7a6376f9eb1
Angel Woo <awoo424@gmail.com>
2022-01-19 20:43:00 +0800

    Add set-type in rolling_join example

--- src/clojask_examples/rolling_join.clj
+++ src/clojask_examples/rolling_join.clj
@@ -5,5 +5,8 @@
     (def x (clojask/dataframe "resources/employees.csv"))
     (def y (clojask/dataframe "resources/employees-workleave.csv"))
 
+    (clojask/set-type x "UpdateDate" "datetime")
+    (clojask/set-type y "UpdateDate" "datetime"))
+
     (clojask/compute (clojask/rolling-join-forward x y ["Employee"] ["Employee"] "UpdateDate" "UpdateDate") 8 "resources/output.csv" :exception false)
     )
