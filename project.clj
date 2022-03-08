1f7c04dfa62375fd2477fa5ec81c948883bd02dc
Sung Ho <sungho.chu8567@gmail.com>
2022-03-08 19:34:51 +0800

    Update to clojask 1.2.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.clojure-finance/clojask "1.2.0"]
+                 [com.github.clojure-finance/clojask "1.2.1"]
                  [org.clojure/core.async "0.4.500"]]
   :main ^:skip-aot clojask-examples.core/foo
   :repl-options {:init-ns clojask-examples.core  ;; change the example you want to run here
