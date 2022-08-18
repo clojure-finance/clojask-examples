29f38c4efcdd58b972429c48326065dce547f4d8
Sung Ho <sungho.chu8567@gmail.com>
2022-08-18 16:19:36 +0800

    Updated for formatting

--- src/clojask_examples/basic_tutorial.clj
+++ src/clojask_examples/basic_tutorial.clj
@@ -1,25 +1,24 @@
 (ns clojask-examples.basic-tutorial
-    (:require [clojask.dataframe :as clojask]
-              [clojure.core.async :as async]))
+  (:require [clojask.dataframe :as clojask]
+            [clojure.core.async :as async]))
 ;; Content Below
-  (defn main
-    []
-    ;; The simple way
-    (def df (clojask/dataframe "resources/employees.csv"))
-    (clojask/print-df df)
-    (clojask/set-type df "Salary" "double")
-    (clojask/filter df "Salary" (fn [salary] (<= salary 800)))
-    (clojask/operate df (fn [salary] (* salary 1.2)) "Salary")
-    (clojask/compute df 8 "resources/output.csv" :exception true)
+(defn main
+  []
+  ;; The simple way
+  (def df (clojask/dataframe "resources/employees.csv"))
+  (clojask/print-df df)
+  (clojask/set-type df "Salary" "double")
+  (clojask/filter df "Salary" (fn [salary] (<= salary 800)))
+  (clojask/operate df (fn [salary] (* salary 1.2)) "Salary")
+  (clojask/compute df 8 "resources/output.csv" :exception true)
 
-    ;; Using the `->` macro:
-    (-> (clojask/dataframe "resources/employees.csv")
-        (clojask/set-type "Salary" "double")
-        (clojask/filter "Salary" (fn [salary] (<= salary 800)))
-        (clojask/operate (fn [salary] (* salary 1.2)) "Salary")
-        (clojask/compute 8 "resources/output.csv" :exception true))
+  ;; Using the `->` macro:
+  (-> (clojask/dataframe "resources/employees.csv")
+      (clojask/set-type "Salary" "double")
+      (clojask/filter "Salary" (fn [salary] (<= salary 800)))
+      (clojask/operate (fn [salary] (* salary 1.2)) "Salary")
+      (clojask/compute 8 "resources/output.csv" :exception true))
 
-    ;; import csv files with no column name header
-    (def df (clojask/dataframe "resources/employees.csv" :have-col false))
-    (clojask/print-df df)
-    )
+  ;; import csv files with no column name header
+  (def df (clojask/dataframe "resources/employees.csv" :have-col false))
+  (clojask/print-df df))
