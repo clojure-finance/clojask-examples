8737782a18703ae8a8c0aacfd9055fe279a0db65
Sung ho <sungho.chu8567@gmail.com>
2022-08-18 16:34:48 +0800

    Formatting fixes for basic_tutorial

--- src/clojask_examples/basic_tutorial.clj
+++ src/clojask_examples/basic_tutorial.clj
@@ -1,31 +1,30 @@
 (ns clojask-examples.basic-tutorial
-    (:require [clojask.dataframe :as ck]
-              [clojask-io.input :refer [read-file]]))
-  
-;; Content Below
-  (defn main
-    []
-    ;; The simple way
-    (def df (ck/dataframe "resources/employees.csv"))
-    (ck/print-df df)
-    (ck/set-type df "Salary" "double")
-    (ck/filter df "Salary" (fn [salary] (<= salary 800)))
-    (ck/operate df (fn [salary] (* salary 1.2)) "Salary")
-    (ck/compute df 8 "outputs/basic.csv")
-    
-    ;; make outputs and input have the same order
-    (ck/compute df 8 "outputs/basic2.csv" :order true)
+  (:require [clojask.dataframe :as ck]
+            [clojask-io.input :refer [read-file]]))
 
-    ;; Using the `->` macro:
-    ;; tsv dataset
-    (-> (ck/dataframe "resources/cats.tsv")
-        (ck/set-type "Weight(kg)" "double")
-        (ck/filter "Weight(kg)" (fn [weight] (<= weight 5)))
-        (ck/compute 8 "outputs/basic3.csv"))
-    
-    ;; or use the clojask-io plugin
-    (-> (ck/dataframe (read-file "resources/cats.tsv" :sep "\t" :stat true :output true))
-        (ck/set-type "Weight(kg)" "double")
-        (ck/filter "Weight(kg)" (fn [weight] (<= weight 5)))
-        (ck/compute 8 "outputs/basic4.tsv"))
-    )
+;; Content Below 
+(defn main
+  []
+  ;; The simple way
+  (def df (ck/dataframe "resources/employees.csv"))
+  (ck/print-df df)
+  (ck/set-type df "Salary" "double")
+  (ck/filter df "Salary" (fn [salary] (<= salary 800)))
+  (ck/operate df (fn [salary] (* salary 1.2)) "Salary")
+  (ck/compute df 8 "outputs/basic.csv")
+
+  ;; make outputs and input have the same order
+  (ck/compute df 8 "outputs/basic2.csv" :order true)
+
+  ;; Using the `->` macro:
+  ;; tsv dataset
+  (-> (ck/dataframe "resources/cats.tsv")
+      (ck/set-type "Weight(kg)" "double")
+      (ck/filter "Weight(kg)" (fn [weight] (<= weight 5)))
+      (ck/compute 8 "outputs/basic3.csv"))
+
+  ;; or use the clojask-io plugin
+  (-> (ck/dataframe (read-file "resources/cats.tsv" :sep "\t" :stat true :output true))
+      (ck/set-type "Weight(kg)" "double")
+      (ck/filter "Weight(kg)" (fn [weight] (<= weight 5)))
+      (ck/compute 8 "outputs/basic4.tsv")))
