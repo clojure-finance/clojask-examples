3dc6039f9fd13cd2de8a8f59611268d1f29893ad
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2022-09-04 15:01:50 +0800

    clojask 1.2.4

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.github.clojure-finance/clojask "1.2.3"]
+                 [com.github.clojure-finance/clojask "1.2.4"]
                  [com.github.clojure-finance/clojask-io "1.0.3"]]
-  :main ^:skip-aot clojask-examples.outer-join/main
+  :main ^:skip-aot clojask-examples.basic-tutorial/main
   :repl-options {:init-ns clojask-examples.timezone  ;; change the example you want to run here
                  :timeout 180000})
